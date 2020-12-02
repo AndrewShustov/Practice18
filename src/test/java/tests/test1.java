ac3508161f20c23e88c02524b7aab1ac812add09
Andrew_Shustov <celllithium@gmail.com>
2020-12-02 19:27:58 +0300

    3 cases commit

--- src/test/java/tests/test1.java
+++ src/test/java/tests/test1.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -17,14 +18,19 @@ public class test1 {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("start-maximized");
+        //chromeOptions.addArguments("--user-data-dir=C:\\Users\\A4F7~1\\AppData\\Local\\Temp\\scoped_dir8948_2027490107\\Default");
+        //chromeOptions.addArguments("--profile-directory=Default");
         webDriver = new ChromeDriver(chromeOptions);
+    }
 
+    @BeforeEach
+    public void setup(){
+        webDriver.get("http://google.com");
+        webDriver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
     }
 
     @Test
     public void case1() {
-        webDriver.get("http://google.com");
-        webDriver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
         webDriver.findElement(By.xpath("//*[@jsname='j93WEe']")).click(); //(
         webDriver.findElement(By.xpath("//*[@jsname='N10B9']")).click(); //1
         webDriver.findElement(By.xpath("//*[@jsname='XSr6wc']")).click(); //+
@@ -38,7 +44,21 @@ public class test1 {
         webDriver.findElement(By.xpath("//*[@jsname='WxTTNd']")).click(); //divide
         webDriver.findElement(By.xpath("//*[@jsname='Ax5wH']")).click(); //5
         webDriver.findElement(By.xpath("//*[@jsname='Pt8tGc']")).click(); //=
+    }
 
+    @Test
+    public void case2() {
+        webDriver.findElement(By.xpath("//*[@jsname='abcgof']")).click(); //6
+        webDriver.findElement(By.xpath("//*[@jsname='WxTTNd']")).click(); //divide
+        webDriver.findElement(By.xpath("//*[@jsname='bkEvMb']")).click(); //0
+        webDriver.findElement(By.xpath("//*[@jsname='Pt8tGc']")).click(); //=
+    }
+
+    @Test
+    public void case3() {
+        webDriver.findElement(By.xpath("//*[@jsname='aN1RFf']")).click(); //sin
+        webDriver.findElement(By.xpath("//*[@jsname='j93WEe']")).click(); //(
+        webDriver.findElement(By.xpath("//*[@jsname='Pt8tGc']")).click(); //=
     }
 
     @AfterAll
