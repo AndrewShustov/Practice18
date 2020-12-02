f9639b94f5e1fa1dba0f376aeed2053f27c3b8d4
Andrew_Shustov <celllithium@gmail.com>
2020-12-02 20:04:58 +0300

    3 cases commit

---
+++ src/test/java/tests/CalcPage.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+public class CalcPage {
+    @FindBy(xpath = ("//*[@jsname='VssY5c']"))
+    private WebElement inf;
+
+    public CalcPage() {
+    }
+
+    public CalcPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    public String geiInf() {
+        return inf.getText();
+    }
+}
--- src/test/java/tests/test1.java
+++ src/test/java/tests/test1.java
@@ -4,14 +4,22 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.openqa.selenium.By.xpath;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class test1 {
     private static WebDriver webDriver;
+
+    private static CalcPage calcPage;
+
 
     @BeforeAll
     public static void init() {
@@ -24,41 +32,42 @@ public class test1 {
     }
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         webDriver.get("http://google.com");
         webDriver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
     }
 
     @Test
     public void case1() {
-        webDriver.findElement(By.xpath("//*[@jsname='j93WEe']")).click(); //(
-        webDriver.findElement(By.xpath("//*[@jsname='N10B9']")).click(); //1
-        webDriver.findElement(By.xpath("//*[@jsname='XSr6wc']")).click(); //+
-        webDriver.findElement(By.xpath("//*[@jsname='lVjWed']")).click(); //2
-        webDriver.findElement(By.xpath("//*[@jsname='qCp9A']")).click(); //)
-        webDriver.findElement(By.xpath("//*[@jsname='YovRWb']")).click(); //*
-        webDriver.findElement(By.xpath("//*[@jsname='KN1kY']")).click(); //3
-        webDriver.findElement(By.xpath("//*[@jsname='pPHzQc']")).click(); //-
-        webDriver.findElement(By.xpath("//*[@jsname='xAP7E']")).click(); //4
-        webDriver.findElement(By.xpath("//*[@jsname='bkEvMb']")).click(); //0
-        webDriver.findElement(By.xpath("//*[@jsname='WxTTNd']")).click(); //divide
-        webDriver.findElement(By.xpath("//*[@jsname='Ax5wH']")).click(); //5
-        webDriver.findElement(By.xpath("//*[@jsname='Pt8tGc']")).click(); //=
+        webDriver.findElement(xpath("//*[@jsname='j93WEe']")).click(); //(
+        webDriver.findElement(xpath("//*[@jsname='N10B9']")).click(); //1
+        webDriver.findElement(xpath("//*[@jsname='XSr6wc']")).click(); //+
+        webDriver.findElement(xpath("//*[@jsname='lVjWed']")).click(); //2
+        webDriver.findElement(xpath("//*[@jsname='qCp9A']")).click(); //)
+        webDriver.findElement(xpath("//*[@jsname='YovRWb']")).click(); //*
+        webDriver.findElement(xpath("//*[@jsname='KN1kY']")).click(); //3
+        webDriver.findElement(xpath("//*[@jsname='pPHzQc']")).click(); //-
+        webDriver.findElement(xpath("//*[@jsname='xAP7E']")).click(); //4
+        webDriver.findElement(xpath("//*[@jsname='bkEvMb']")).click(); //0
+        webDriver.findElement(xpath("//*[@jsname='WxTTNd']")).click(); //divide
+        webDriver.findElement(xpath("//*[@jsname='Ax5wH']")).click(); //5
+        webDriver.findElement(xpath("//*[@jsname='Pt8tGc']")).click(); //=
     }
 
     @Test
     public void case2() {
-        webDriver.findElement(By.xpath("//*[@jsname='abcgof']")).click(); //6
-        webDriver.findElement(By.xpath("//*[@jsname='WxTTNd']")).click(); //divide
-        webDriver.findElement(By.xpath("//*[@jsname='bkEvMb']")).click(); //0
-        webDriver.findElement(By.xpath("//*[@jsname='Pt8tGc']")).click(); //=
+        webDriver.findElement(xpath("//*[@jsname='abcgof']")).click(); //6
+        webDriver.findElement(xpath("//*[@jsname='WxTTNd']")).click(); //divide
+        webDriver.findElement(xpath("//*[@jsname='bkEvMb']")).click(); //0
+        webDriver.findElement(xpath("//*[@jsname='Pt8tGc']")).click(); //=
+        assertEquals("Infinity", calcPage.geiInf());
     }
 
     @Test
     public void case3() {
-        webDriver.findElement(By.xpath("//*[@jsname='aN1RFf']")).click(); //sin
-        webDriver.findElement(By.xpath("//*[@jsname='j93WEe']")).click(); //(
-        webDriver.findElement(By.xpath("//*[@jsname='Pt8tGc']")).click(); //=
+        webDriver.findElement(xpath("//*[@jsname='aN1RFf']")).click(); //sin
+        webDriver.findElement(xpath("//*[@jsname='j93WEe']")).click(); //(
+        webDriver.findElement(xpath("//*[@jsname='Pt8tGc']")).click(); //=
     }
 
     @AfterAll
